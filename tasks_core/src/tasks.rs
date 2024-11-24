5d1ab07ce9de06fafe8483e36f0a2d9009004aca
slothytoppy <jacobcalvining@gmail.com>
2024-11-23 22:55:05 -0600

    [feat] serialization, parsing status

--- tasks_core/src/tasks.rs
+++ tasks_core/src/tasks.rs
@@ -1,5 +1,103 @@
 use std::{fmt::Display, path::PathBuf};
 
+#[derive(Debug, PartialEq, Eq)]
+pub enum TaskError {
+    NoFile(String),
+    NoData,
+    ParseError((char, usize)),
+}
+
+impl Display for TaskError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            TaskError::ParseError((ch, idx)) => {
+                write!(f, "Failed to parse: {} at index {}", ch, idx)
+            }
+            TaskError::NoFile(file) => write!(f, "ENOFILE: {file}"),
+            TaskError::NoData => write!(f, "No data to parse"),
+        }
+    }
+}
+
+fn open<P: AsRef<std::path::Path>>(file: P) -> std::io::Result<std::fs::File> {
+    std::fs::File::open(file)
+}
+
+fn parse_header(data: &str) -> Result<(&str, usize), TaskError> {
+    println!("from parse_header");
+    //println!("{data:?}");
+    if data.is_empty() {
+        return Err(TaskError::NoData);
+    }
+    let mut start = 1;
+
+    for (i, c) in data.chars().enumerate() {
+        match c {
+            '[' => start = i + 1,
+            ']' => {
+                //println!("{i:?} {}", &data[1..=i.saturating_sub(1)]);
+                return Ok((&data[start..=i.saturating_sub(1)], i + 1));
+            }
+            _ => {}
+        }
+    }
+
+    Err(TaskError::ParseError((
+        data.chars().next().unwrap_or_default(),
+        data.len(),
+    )))
+}
+
+fn parse_status(data: &str) -> Result<(bool, usize), TaskError> {
+    println!("from parse_status");
+
+    if data.is_empty() {
+        return Err(TaskError::NoData);
+    }
+
+    for (i, c) in data.chars().enumerate() {
+        match c {
+            '0' => return Ok((false, i + 1)),
+            '1' => return Ok((true, i + 1)),
+            ' ' | '\n' | '\t' => continue,
+            _ => {
+                println!("{}", data);
+                return Err(TaskError::ParseError((
+                    data.chars().next().unwrap_or_default(),
+                    i,
+                )));
+            }
+        }
+    }
+
+    Err(TaskError::NoData)
+}
+
+fn parse_data(data: &str) -> Result<(&str, usize), TaskError> {
+    println!("from parse_data");
+    //println!("{data:?}");
+    if data.is_empty() {
+        return Err(TaskError::NoData);
+    }
+
+    for (i, c) in data.chars().enumerate() {
+        match c {
+            '[' => {
+                //println!("{i:?} {:?}", &data[0..i]);
+                return Ok((data[0..i].trim(), i.saturating_sub(1)));
+            }
+            ']' => {
+                return Err(TaskError::ParseError((
+                    data.chars().next().unwrap_or_default(),
+                    data.len(),
+                )))
+            }
+            _ => {}
+        };
+    }
+    Ok((data, data.len()))
+}
+
 #[derive(Default, Debug)]
 pub struct Task<'a> {
     data: &'a str,
@@ -8,12 +106,8 @@ pub struct Task<'a> {
 }
 
 impl<'a> Task<'a> {
-    pub fn new(data: &'a str, name: &'a str) -> Self {
-        Self {
-            data,
-            name,
-            status: false,
-        }
+    pub fn new(data: &'a str, name: &'a str, status: bool) -> Self {
+        Self { data, name, status }
     }
 
     pub fn data(&self) -> &'a str {
@@ -42,117 +136,6 @@ pub struct TaskList<'a> {
     list: Vec<Task<'a>>,
 }
 
-#[derive(Debug, PartialEq, Eq)]
-pub enum TaskError {
-    ENOFILE(String),
-    NoData,
-    ParseError(u32),
-}
-
-impl Display for TaskError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            TaskError::ParseError(c) => write!(f, "ParseError at char {c}"),
-            TaskError::ENOFILE(file) => write!(f, "ENOFILE: {file}"),
-            TaskError::NoData => write!(f, "No data to parse"),
-        }
-    }
-}
-
-fn open<P: AsRef<std::path::Path>>(file: P) -> std::io::Result<std::fs::File> {
-    std::fs::File::open(file)
-}
-
-//fn parse(data: &str) -> Result<Task, TaskError> {
-//    if data.is_empty() {
-//        return Err(TaskError::NoData);
-//    }
-//
-//    let mut task = Task::default();
-//    let mut has_start = false;
-//    let mut has_end = false;
-//    let mut pos = 0;
-//    let data = data.trim();
-//
-//    for (i, c) in data.chars().enumerate() {
-//        if !has_end {
-//            match c {
-//                ' ' => continue,
-//                '[' => has_start = true,
-//                ']' => {
-//                    if !has_start {
-//                        return Err(TaskError::ParseError(i as u32 + 1));
-//                    }
-//                    task.name = &data[1..=i - 1];
-//                    has_end = true;
-//                    pos = i + 1;
-//                }
-//                _ => {}
-//            }
-//        }
-//    }
-//
-//    println!("{data:?}");
-//
-//    if data[pos..].is_empty() {
-//        return Err(TaskError::NoData);
-//    }
-//
-//    task.name = &data[1..=pos.saturating_sub(2)];
-//    task.data = data[pos..].trim();
-//
-//    Ok(task)
-//}
-
-fn parse_header(data: &str) -> Result<(&str, usize), TaskError> {
-    println!("from parse_header");
-    //println!("{data:?}");
-    if data.is_empty() {
-        return Err(TaskError::NoData);
-    }
-    let mut start = 1;
-
-    for (i, c) in data.chars().enumerate() {
-        match c {
-            '[' => start = i + 1,
-            ']' => {
-                //println!("{i:?} {}", &data[1..=i.saturating_sub(1)]);
-                return Ok((&data[start..=i.saturating_sub(1)], i + 1));
-            }
-            _ => {}
-        }
-    }
-
-    Err(TaskError::ParseError(420000))
-}
-
-fn parse_data(data: &str) -> Result<(&str, usize), TaskError> {
-    println!("from parse_data");
-    //println!("{data:?}");
-    if data.is_empty() {
-        return Err(TaskError::NoData);
-    }
-
-    for (i, c) in data.chars().enumerate() {
-        match c {
-            //'\t' | '\n' => start += 1,
-            //'!'..='Z' | '\\' | '^'..='~' => found_alphanum = true,
-            //' ' => {
-            //    if !found_alphanum {
-            //        start += 1;
-            //    }
-            //}
-            '[' => {
-                //println!("{i:?} {:?}", &data[0..i]);
-                return Ok((data[0..i].trim(), i.saturating_sub(1)));
-            }
-            ']' => return Err(TaskError::ParseError(i as u32)),
-            _ => {}
-        };
-    }
-    Ok((data, data.len()))
-}
-
 impl<'a> TaskList<'a> {
     pub fn get(&self, idx: usize) -> Option<&Task<'a>> {
         self.list.get(idx)
@@ -170,7 +153,18 @@ impl<'a> TaskList<'a> {
         self.list.insert(idx, task)
     }
 
-    pub fn serialize(&self, file: PathBuf) {}
+    pub fn serialize(&self, file: PathBuf) {
+        let mut buff = String::default();
+        self.list.iter().for_each(|task| {
+            buff.push_str(&format!(
+                "[{}]\n{} {}\n",
+                task.name(),
+                task.status(),
+                task.data()
+            ));
+        });
+        let _ = std::fs::write(file, buff);
+    }
 
     pub fn deserialize(data: &'a str) -> Result<Self, TaskError> {
         if data.is_empty() {
@@ -192,14 +186,24 @@ impl<'a> TaskList<'a> {
                     }
                 }
             };
-            //let (name, new_pos) = parse_header(&data[pos..])?;
+            pos += new_pos;
+            let (status, new_pos) = parse_status(&data[pos..])?;
             pos += new_pos;
             let (data, new_pos) = parse_data(&data[pos..])?;
             pos += new_pos;
-            list.push(Task::new(data, name));
+            list.push(Task::new(data, name, status));
         }
 
         Ok(list)
+    }
+}
+
+impl<'a> Display for TaskList<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        self.list.iter().for_each(|task: &Task<'a>| {
+            let _ = write!(f, "[{}]\n{}\n", task.name(), task.name());
+        });
+        Ok(())
     }
 }
 
@@ -210,16 +214,24 @@ mod test {
     #[test]
     pub fn test_list() {
         let data = r#"[hai]
+            0
             bai
             [bai]
+            1
             urmom
             [urmom]
+            1
             hai"#;
 
-        match TaskList::deserialize(data) {
-            Ok(list) => println!("{list:?}"),
-            Err(e) => println!("{e:?}"),
-        }
+        let list = match TaskList::deserialize(data) {
+            Ok(list) => list,
+            Err(e) => panic!("{e}"),
+        };
+
+        //let Ok(path) = PathBuf::from_str("urmom.tl");
+        //list.serialize(path);
+
+        println!("{list:?}");
 
         panic!();
     }
